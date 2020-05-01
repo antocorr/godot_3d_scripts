6fa58c19f2d3728e6e5a13d28ccac958c3444e19
antocors <navylon@gmail.com>
2020-05-01 10:19:54 +0200

    Add files via upload

--- culling.gd
+++ culling.gd
@@ -14,14 +14,71 @@ export var tres = 7
 var counter = 0
 export var debug = ""
 export var reset_visibility = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var create_culling_grid = false
+export var remove_culling_grid = false
+export var grid_size = 20
+export var delete_grid_parents = false
+var cgrid = {}
 
 
-# Called when the node enters the scene tree for the first time.
+func __set_owner(p, ow):
+	p.set_owner(ow)
+	if p.get_child_count() > 0:
+		for c in p.get_children():
+			__set_owner(c, ow)
+func do_create_culling_grid(par = null):
+	if !par:
+		par = self
+	for c in par.get_children():
+		var t = c.translation
+		var gx = round(t.x / grid_size) * grid_size
+		var gy = round(t.y / grid_size) * grid_size
+		var gz = round(t.z / grid_size) * grid_size
+		if str(gx) == "-0":
+			gx= 0
+		if str(gy) == "-0":
+			gy = 0
+		if str(gz) == "-0":
+			gz = 0
+		var gname = str(gx).replace("-", "m") + "_" + str(gy).replace("-", "m") + "_" + str(gz).replace("-", "m")
+		var g = null
+		if !par.has_node(gname):
+			g = Spatial.new()
+			g.name = gname
+			g.translation =  Vector3(gx, gy, gz)
+			par.add_child(g)
+			g.set_owner(get_parent())			
+		else:
+			g = par.get_node(gname)
+		c.get_parent().remove_child(c)
+		g.add_child(c)
+		__set_owner(c, get_parent())
+		c.translation = t - g.translation
+		
+func do_remove_grid():
+	for c in get_children():
+		do_reparent_grid_el(c)
+		if delete_grid_parents:
+			c.free()
+func do_reparent_grid_el(par):
+	for c in par.get_children():
+		c.get_parent().remove_child(c)
+		add_child(c)
+		__set_owner(c, get_parent())
+		c.translation += par.translation
 func _ready():
-	pass # Replace with function body.
+	if create_culling_grid:
+		create_culling_grid = false
+		var par = null
+		if starting_path == "":
+			par = self
+		else:
+			par = get_node(starting_path)
+		if depth:
+			for n in par.get_children():
+				do_create_culling_grid(n)
+		else:
+			do_create_culling_grid()
 
 func hide_if_out(nod, t):
 	if !visibility_original.get(nod.get_path()):
@@ -32,7 +89,7 @@ func hide_if_out(nod, t):
 	else:
 		nod.show()
 		visibility_enabled.append(nod.get_path())
-func _process(delta):
+func _process(_delta):
 	counter += 1
 	var t = null
 	if tracked_p:
@@ -48,6 +105,21 @@ func _process(delta):
 					nn.show()
 			else:
 				n.show()
+	if remove_culling_grid and run_in_editor and Engine.editor_hint:
+		remove_culling_grid = false
+		do_remove_grid()
+	if create_culling_grid and run_in_editor and Engine.editor_hint:
+		create_culling_grid = false
+		var par = null
+		if starting_path == "":
+			par = self
+		else:
+			par = get_node(starting_path)
+		if depth:
+			for n in par.get_children():
+				do_create_culling_grid(n)
+		else:
+			do_create_culling_grid()
 	if active and t and counter > interval_btw_checks:
 		counter = 0
 		if Engine.editor_hint and !run_in_editor:
@@ -55,7 +127,7 @@ func _process(delta):
 		visibility_enabled = []
 		for tt in t.get_children():
 			if positions.get(tt.name):
-				if (tt.translation - positions[tt.name]).length() < tres:
+				if (tt.translation - positions[tt.name]).length() < tres and !Engine.editor_hint:
 					continue
 			positions[tt.name] = tt.translation
 			var par = null
